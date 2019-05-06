5a37384ea9d4af6d00c22c0170c1a3ad302d89e7
alperkaradag <33064364+alperkaradag@users.noreply.github.com>
2019-05-06 21:38:14 +0300

    asd

--- src/sample/Pawn.java
+++ src/sample/Pawn.java
@@ -1,10 +1,18 @@
 package sample;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Pawn {
 
     public Cell c;
     public int direction = 0;
     public int team;
+    public ImageView pawnImage;
 
     public Pawn( Cell c, int team ) {
         this.c = c;
@@ -12,10 +20,16 @@ public class Pawn {
         this.team = team;
     }
 
-    public void draw() {
+    public void draw(GridPane root) throws FileNotFoundException {
         double x = c.getX();
         double y = c.getY();
+        if(team == 1){
+            pawnImage = new ImageView(new Image(new FileInputStream("C:\\Users\\alper\\IdeaProjects\\Command\\assets\\bluerobot.png")));
+        }
+        else{
+            pawnImage = new ImageView(new Image(new FileInputStream("C:\\Users\\alper\\IdeaProjects\\Command\\assets\\redrobot.png")));
+        }
 
+        root.getChildren().addAll(pawnImage);
     }
-
 }
